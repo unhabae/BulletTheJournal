05211fd3c8e69ed25c9c45c24014069d7e9e5a5c
unhabae <158564666+unhabae@users.noreply.github.com>
2024-05-05 18:43:41 +0200

    js

--- journal.js
+++ journal.js
@@ -2,3 +2,12 @@ const ukedager = ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"]
 
 const date = new Date()
 console.log( ukedager[date.getDay()])
+
+const weekElm = document.getElementById("week")
+
+for (i = 0; i<7;i++) {
+    const div = document.createElement("div")
+    div.innerHTML = ukedager[ (i+1)%7 ]
+    weekElm.appendChild(div)
+}
+
