b0387735e7f9d473a9cf5d05fe60966d0bf3c4b2
unhabae <158564666+unhabae@users.noreply.github.com>
2024-05-05 19:31:52 +0200

    change name index

--- journal.js
+++ journal.js
@@ -1,13 +1,21 @@
-const ukedager = ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"]
+const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-const date = new Date()
-console.log( ukedager[date.getDay()])
+// U K E D A G E R 
+// Henter containeren
+const weekElm = document.getElementById("weekDiv")
 
-const weekElm = document.getElementById("week")
-
+// Ukedagene: 
 for (i = 0; i<7;i++) {
     const div = document.createElement("div")
-    div.innerHTML = ukedager[ (i+1)%7 ]
+    div.innerHTML = weekdays[ (i+1)%7 ]     //går det over 7, henter 0
     weekElm.appendChild(div)
+    div.setAttribute('id',(i+1)%7);
 }
+
+// Finner dagen idag
+const date = new Date()
+console.log( weekdays[date.getDay()])
+const today = document.getElementById(date.getDay())
+today.style.backgroundColor = "#7f8da1"
+today.style.color = "white"
 
